af2f1dd23271839ebd5aa5ac1e9b7f970e5453e7
Abella Maya Santi <abellajoesni@gmail.com>
2025-07-28 14:42:57 +0700

    removed scotland from main travels page

--- src/app/travels/page.tsx
+++ src/app/travels/page.tsx
@@ -9,7 +9,7 @@ export default function TravelsPage() {
   const destinations = [
     { name: "Austria", image: "/images/travel/cam1.png", href: "/travels/austria" },
     { name: "Bosnia", image: "/images/travel/cam2.png", href: "/travels/bosnia" },
-    { name: "Hungary", image: "/images/travel/cam3.png", href: "/travels/budapest" },
+    { name: "Budapest", image: "/images/travel/cam3.png", href: "/travels/budapest" },
     { name: "Croatia", image: "/images/travel/cam4.png", href: "/travels/croatia" },
     { name: "Egypt", image: "/images/travel/cam5.png", href: "/travels/egypt" },
     { name: "Germany", image: "/images/travel/cam6.png", href: "/travels/germany" },
@@ -18,12 +18,11 @@ export default function TravelsPage() {
     { name: "Italy", image: "/images/travel/cam9.png", href: "/travels/italy" },
     { name: "Korea", image: "/images/travel/don1.png", href: "/travels/korea" },
     { name: "Netherlands", image: "/images/travel/neth.png", href: "/travels/neth" },
-    { name: "Czech Republic", image: "/images/travel/don2.png", href: "/travels/czech-republic" },
-    { name: "Scotland", image: "/images/travel/don3.png", href: "/travels/scot" },
+    { name: "Prague", image: "/images/travel/don2.png", href: "/travels/prague" },
     { name: "Serbia", image: "/images/travel/don4.png", href: "/travels/serbia" },
     { name: "Spain", image: "/images/travel/don5.png", href: "/travels/spain" },
     { name: "Turkey", image: "/images/travel/bridge1.png", href: "/travels/turkey" },
-    { name: "United Kingdom", image: "/images/travel/london2.png", href: "/travels/uk" },
+    { name: "UK", image: "/images/travel/bridge2.png", href: "/travels/uk" },
     { name: "USA", image: "/images/travel/tate.png", href: "/travels/usa" },
   ];
 
